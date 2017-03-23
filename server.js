1f37085702d3bdff5813948d1dbd0827ca391cfa
Kyle R <kyle.rinehart83@gmail.com>
2017-03-23 18:43:22 -0400

    fixed server, now running off of online DB, routes fixed as well

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-var mongojs = require('mongojs');
+var mongoos = require('mongoose');
+
+//Require Schemas
+var Article = require('./server/model.js');
 
 // Create Instance of Express
 var app = express();
-var PORT = process.env.PORT || 8080; // Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 8080; // Sets an initial port for the listener
 
 // Run Morgan for Logging
 app.use(logger('dev'));
@@ -19,16 +22,19 @@ app.use(express.static('./public'));
 
 // -------------------------------------------------
 
-// MongoDB Configuration configuration (Change this URL to your own DB)
-var databaseUrl = 'NYT articles';
-var collections = ["searches"];
+// MongoDB Configuration configuration
+mongoose.connect('mongodb://heroku_7zv5tzm5:heroku_7zv5tzm5@ds139430.mlab.com:39430/heroku_7zv5tzm5');
+var db = mongoose.connection;
 
-// use mongojs to hook the database to the db variable 
-var db = mongojs(databaseUrl, collections);
-
+// Log errors
 db.on('error', function (err) {
-  console.log('MongoDB Error: ', err);
+  console.log('Mongoose Error: ', err);
 });
+
+// Log successful connection
+db.once('open', function() {
+	console.log('Mongoose connection successful.')
+})
 
 // -------------------------------------------------
 
@@ -38,12 +44,65 @@ app.get('/', function(req, res){
   res.sendFile('./public/index.html');
 });
 
-// This is the route we will send POST requests to save each search.
-// app.post('/api/', function(req, res){
-//   console.log("BODY: " + req.body);
+// Route to get all the saved articles
+app.get('/api/saved', function(req,res) {
 
-//   // Here we'll save the location based on the JSON input. 
-//   // We'll use Date.now() to always get the current date time
+	Article.find({})
+		.exec(function(err, doc) {
+
+			if(err) {
+				console.log(err);
+			} else {
+				res.send(doc);
+			}
+		})
+})
+
+// This is the route we will send POST requests to save each search.
+app.post('/api/saved', function(req, res){
+	var newArticle = new Article(req.body);
+	//test data collection
+    console.log("BODY: " + req.body);
+
+    var title = req.body.title;
+    var date = req.body.date;
+    var url = req.body.url;
+
+    newArticle = save.(function(err, doc){
+    	if(err){
+    		console.log(err);
+    	} else {
+    		res.send(doc._id);
+    	}
+    });
+
+});
+
+// Route to delete an article from the saved list
+app.delete('/api/saved/', function(req, res){
+
+	var url = req.param('url');
+
+	Article.find({"url": url}).remove().exec(function(err, data){
+		if(err){
+			console.log(err);
+		}
+		else {
+			res.send("Deleted");
+		}
+	});
+});
+
+// -------------------------------------------------
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+
+  // Here we'll save the location based on the JSON input. 
+  // We'll use Date.now() to always get the current date time
 //   db.history.insert({"title": req.body.title, "date": Date.now(), "url": req.body.url}, function(err){
 //     if(err){
 //       console.log(err);
@@ -53,10 +112,3 @@ app.get('/', function(req, res){
 //     }
 //   })
 // });
-
-// -------------------------------------------------
-
-// Listener
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
