2cc50566e0efc3e482f1e7ccca8d23af2af4292a
Kyle R <kyle.rinehart83@gmail.com>
2017-03-23 20:29:48 -0400

    added missing e to mongoose require statement

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-var mongoos = require('mongoose');
+var mongoose = require('mongoose');
 
 //Require Schemas
 var Article = require('./server/model.js');
